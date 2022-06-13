a5c6b2d626dcfd6136a669d32f8373686a22108c
imtusharchavan <tusharrchavan014@gmail.com>
2022-06-13 12:05:09 +0530

    Create admin

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from student.models import User, Student, Faculty, Course, Semester, Subject
+
+admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Faculty)
+admin.site.register(Course)
+admin.site.register(Semester)
+admin.site.register(Subject)
