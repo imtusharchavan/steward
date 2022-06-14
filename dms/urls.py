218eb4a6c6e5465e0a39fde1742bb357f3783046
imtusharchavan <tusharrchavan014@gmail.com>
2022-06-14 18:39:59 +0530

    Add static files

--- dms/urls.py
+++ dms/urls.py
@@ -1,9 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from student.views import landing
+from student.views import LandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", landing),
+    path("", LandingPageView.as_view(), name='landing-page'),
     path('student/', include('student.urls', namespace='student')),
 ]
+
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
