4234509fd99c54847a41c942d159222a154c2ca2
imtusharchavan <tusharrchavan014@gmail.com>
2022-06-19 08:12:56 +0530

    Fix bug

--- classrooms/urls.py
+++ classrooms/urls.py
@@ -1,15 +1,16 @@
-# from django.urls import path
+from django.urls import path
 
-# from .views import ClassroomListView, ClassroomDetailView, ClassroomCreateView, ClassroomUpdateView, ClassroomDeleteView
-
-
-# app_name = "classrooms"
+from .views import ClassroomListView, ClassroomCreateView, ClassroomDetailView, ClassroomUpdateView, ClassroomDeleteView
 
 
-# urlpatterns = [
-#     path('', ClassroomListView.as_view(), name='classroom-list'),
-#     path('<int:pk>/', ClassroomDetailView.as_view(), name='classroom-detail'),
-#     path('<int:pk>/update/', ClassroomUpdateView.as_view(), name='classroom-update'),
-#     path('<int:pk>/delete/', ClassroomDeleteView.as_view(), name='classroom-delete'),
-#     path('create/', ClassroomCreateView.as_view(), name='classroom-create'),
-# ]
+app_name = "classrooms"
+
+
+urlpatterns = [
+    path('', ClassroomListView.as_view(), name='classroom-list'),
+    path('create/', ClassroomCreateView.as_view(), name='classroom-create'),
+    path('<int:pk>/', ClassroomDetailView.as_view(), name='classroom-detail'),
+    path('<int:pk>/update/', ClassroomUpdateView.as_view(), name='classroom-update'),
+    path('<int:pk>/delete/', ClassroomDeleteView.as_view(), name='classroom-delete'),
+    
+]
--- classrooms/views.py
+++ classrooms/views.py
@@ -1,57 +1,93 @@
-# from django.shortcuts import render, redirect, reverse
-# from django.contrib.auth.mixins import LoginRequiredMixin
-# from django.views import generic
-# from users.mixins import FacultyAndLoginRequiredMixin
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from users.mixins import FacultyAndLoginRequiredMixin
 
-# from users.models import Classroom
-# from .forms import ClassroomModelForm
+from users.models import Classroom
+from classrooms.forms import ClassroomModelForm
 
 
+class ClassroomListView(FacultyAndLoginRequiredMixin, generic.ListView):
+    template_name = "classrooms/classroom_list.html"
+    context_object_name = "classrooms"
 
-# class ClassroomListView(LoginRequiredMixin, generic.ListView):
-#     template_name = "classrooms/classroom_list.html"
+    def get_queryset(self):
+        user = self.request.user
+        # initial queryset of classrooms for particular teacher
+        if user.is_faculty:
+            queryset = Classroom.objects.filter(teacher=user.userprofile)
+        else:
+            queryset = Classroom.objects.filter(teacher=user.student.classrooms.people)
+            # filter for the student that  is logged in
+            queryset = queryset.filter(student__user=user)
+        return queryset
    
-#     def get_queryset(self):
-#         teacher = self.request.user.userprofile
-#         return Classroom.objects.filter(teacher=teacher)
-#     context_object_name = "classrooms"
+    # def get_queryset(self):
+    #     teacher = self.request.user.userprofile
+    #     return Classroom.objects.filter(teacher=teacher)
 
 
-# class ClassroomDetailView(FacultyAndLoginRequiredMixin, generic.DetailView):
-#     template_name = "classrooms/classroom_detail.html"
-
-#     def get_queryset(self):
-#         teacher = self.request.user.userprofile
-#         return Classroom.objects.filter(teacher=teacher)
-#     context_object_name = "classroom"
-
-
-# class ClassroomCreateView(FacultyAndLoginRequiredMixin, generic.CreateView):
-#     template_name = "classrooms/classroom_create.html"
-#     form_class = ClassroomModelForm
+class ClassroomCreateView(FacultyAndLoginRequiredMixin, generic.CreateView):
+    template_name = "classrooms/classroom_create.html"
+    form_class = ClassroomModelForm
     
-#     def get_success_url(self) :
-#         return reverse("classrooms:classroom-list")
-
-
-# class ClassroomUpdateView(FacultyAndLoginRequiredMixin, generic.UpdateView):
-#     template_name = "classrooms/classroom_update.html"
-#     form_class = ClassroomModelForm
+    def get_success_url(self) :
+        return reverse("classrooms:classroom-list")
     
-#     def get_success_url(self) :
-#         return reverse("classrooms:classroom-list")
+    def form_valid(self, form):
+        classroom = form.save(commit=False)
+        classroom.teacher = self.request.user.userprofile
+        classroom.save()
+        
+        return super(ClassroomCreateView, self).form_valid(form)
 
-#     def get_queryset(self):
-#         teacher = self.request.user.userprofile
-#         return Classroom.objects.filter(teacher=teacher)
+
+class ClassroomDetailView(FacultyAndLoginRequiredMixin, generic.DetailView):
+    template_name = "classrooms/classroom_detail.html"
+    context_object_name = "classroom"
+
+    def get_queryset(self):
+        user = self.request.user
+        # initial queryset of classrooms for particular teacher
+        if user.is_faculty:
+            queryset = Classroom.objects.filter(teacher=user.userprofile)
+        else:
+            queryset = Classroom.objects.filter(teacher=user.student.classrooms.people)
+            # filter for the student that  is logged in
+            queryset = queryset.filter(student__user=user)
+        return queryset
+
+    # def get_queryset(self):
+    #     teacher = self.request.user.userprofile
+    #     return Classroom.objects.filter(teacher=teacher)
 
 
-# class ClassroomDeleteView(FacultyAndLoginRequiredMixin, generic.DeleteView):
-#     template_name = "classrooms/classroom_delete.html"
-
-#     def get_success_url(self) :
-#         return reverse("classrooms:classroom-list")
+class ClassroomUpdateView(FacultyAndLoginRequiredMixin, generic.UpdateView):
+    template_name = "classrooms/classroom_update.html"
+    form_class = ClassroomModelForm
     
-#     def get_queryset(self):
-#         teacher = self.request.user.userprofile00
-#         return Classroom.objects.filter(teacher=teacher)
+    def get_success_url(self) :
+        return reverse("classrooms:classroom-list")
+    
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     return Classroom.objects.filter(teacher=user.userprofile)
+
+    def get_queryset(self):
+        teacher = self.request.user.userprofile
+        return Classroom.objects.filter(teacher=teacher)
+
+
+class ClassroomDeleteView(FacultyAndLoginRequiredMixin, generic.DeleteView):
+    template_name = "classrooms/classroom_delete.html"
+
+    def get_success_url(self) :
+        return reverse("classrooms:classroom-list")
+
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     return Classroom.objects.filter(teacher=user.userprofile)
+    
+    def get_queryset(self):
+        teacher = self.request.user.userprofile
+        return Classroom.objects.filter(teacher=teacher)
