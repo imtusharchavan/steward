3d069a3c9d768fe89d9ba7c48ef39da79cf60522
imtusharchavan <tusharrchavan014@gmail.com>
2022-06-19 09:12:24 +0530

    Set env variables

--- dms/settings.py
+++ dms/settings.py
@@ -1,17 +1,21 @@
 from pathlib import Path
 
+import environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env()
+
+DEBUG = env('DEBUG')
+
+SECRET_KEY = env('SECRET_KEY')
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ng76l*jq2%rfngh&cws#9=j0t+v6h^1vf)-2e8xxik8uvf@wr7'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
